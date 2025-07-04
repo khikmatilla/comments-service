a690c471b953b5e7bd7b3914e69cdbff3bfb12cb
hikmatilla <xabibullayev.xikmatilla@zaytuntech.uz>
2025-07-04 11:33:16 +0500

    initial commit

---
+++ src/test/java/uz/pdp/commentsservise/CommentsServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.commentsservise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsServiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
